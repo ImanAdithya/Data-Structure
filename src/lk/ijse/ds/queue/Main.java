7e3145ee188a5d1dec3a682fb2f746350585c71a
ImanAdithya <adithyaiman07@gmail.com>
2023-11-23 11:02:52 +0530

    content updated

---
+++ src/lk/ijse/ds/queue/Main.java
@@ -0,0 +1,19 @@
+package lk.ijse.ds.queue;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Queue queue = new Queue (5);
+
+        queue.enqueue(10);
+        queue.enqueue(20);
+        queue.enqueue(30);
+
+        queue.printQueue();
+
+        queue.deQueue();
+
+        queue.printQueue();
+
+    }
+}
--- src/lk/ijse/ds/stack/Stack.java
+++ src/lk/ijse/ds/stack/Stack.java
@@ -2,66 +2,66 @@ package lk.ijse.ds.stack;
 
 public class Stack {
 
-   private int [] elementData;
-   private int top;//point to the of the stack(last Element)
+    private int[] elementData;
+    private int top;//point to the of the stack(last Element)
 
-   public Stack(final int initialCapacity){
-       elementData=new int[initialCapacity];
-       top=-1;
-   }
+    public Stack(final int initialCapacity) {
+        elementData = new int[initialCapacity];
+        top = -1;
+    }
 
-   public void push(int data){
-       if (isFull ()){
-           throw new RuntimeException ("Stack is full..");
-       }
-       elementData[++top]=data;
-   }
+    public void push(int data) {
+        if (isFull ()) {
+            throw new RuntimeException ("Stack is full..");
+        }
+        elementData[++top] = data;
+    }
 
 
     public void printStack() {
         System.out.print ("[");
         for (int i = 0; i <= top; i++) {
-            System.out.print (elementData[i]+",");
+            System.out.print (elementData[i] + ",");
         }
-        System.out.print (isEmpty ()?"Empty Stack]":"\b]\n");
+        System.out.print (isEmpty () ? "Empty Stack]" : "\b]\n");
     }
 
 
     public int pop() {
-       if (isEmpty ()){
-           System.err.println ("Stack is Empty");
-       }
+        if (isEmpty ()) {
+            System.err.println ("Stack is Empty");
+        }
 
         return elementData[top--];
     }
 
     public boolean isEmpty() {
-       return top>0;
+        return top > 0;
     }
 
     public int peek() {
-       return elementData[top];
+        return elementData[top];
     }
 
     public boolean isFull() {
-       return elementData.length-1==top;
+        return elementData.length - 1 == top;
     }
 
     public int size() {
-       return top+1;
+        return top + 1;
     }
 
-    public void clear(){
-       top=-1;
+    public void clear() {
+        top = -1;
     }
 
-    public void grow(){
-       int [] temp=elementData;
+    public void grow() {
+        int[] temp = elementData;
 
-       elementData=new int[elementData.length * 2];
+        elementData = new int[elementData.length * 2];
 
         for (int i = 0; i < temp.length; i++) {
-            elementData[i]=temp[i];
+            elementData[i] = temp[i];
         }
     }
 }
