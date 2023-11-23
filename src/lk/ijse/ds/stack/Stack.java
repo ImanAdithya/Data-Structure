710c0e7c68d3eb664a3d2b1283f4818da261f7ae
ImanAdithya <adithyaiman07@gmail.com>
2023-11-23 09:52:59 +0530

    first commit

---
+++ src/lk/ijse/ds/stack/Stack.java
@@ -0,0 +1,50 @@
+package lk.ijse.ds.stack;
+
+public class Stack {
+
+   private int [] elementData;
+   private int top;//point to the of the stack(last Element)
+
+   public Stack(final int initialCapacity){
+       elementData=new int[initialCapacity];
+       top=-1;
+   }
+
+   public void push(int data){
+       elementData[++top]=data;
+   }
+
+
+    public void printStack() {
+        System.out.print ("[");
+        for (int i = 0; i <= top; i++) {
+            System.out.print (elementData[i]+",");
+        }
+        System.out.print ("\b]\n");
+    }
+
+
+    public int pop() {
+        return elementData[top--];
+    }
+
+    public boolean isEmpty() {
+       return top>0;
+    }
+
+    public int peek() {
+       return elementData[top];
+    }
+
+    public boolean isFull() {
+       return elementData.length==top;
+    }
+
+    public int size() {
+       return elementData.length;
+    }
+
+    public void clear(){
+       top=-1;
+    }
+}
