86f0ea0a6393e63f10bdbea8b2e561d6b73358f8
ImanAdithya <adithyaiman07@gmail.com>
2023-11-23 14:19:48 +0530

    content updated

--- src/lk/ijse/ds/linkedlist/LinkedList.java
+++ src/lk/ijse/ds/linkedlist/LinkedList.java
@@ -1,13 +1,70 @@
 package lk.ijse.ds.linkedlist;
 
 public class LinkedList {
+
+    private Node head;
     private static class Node{
         private int data;
-        private int next;
+        private Node next;
         public Node(int data){
             this.data=data;
         }
     }
+    public void insertAtBeginning(int data) {
+        Node node = new Node (data);
 
-    private Node head;
+        node.next=head;
+        head=node;
+    }
+
+    public void insertAtEnding(int data) {
+
+        if (head==null){
+            insertAtBeginning (data);
+            return;
+        }
+
+        Node node = new Node(data);
+
+       Node temp=head;
+
+        while (temp.next!=null){
+            temp=temp.next;
+        }
+
+        temp.next=node;
+
+    }
+
+    public void traverse(){
+        System.out.print ("[");
+
+        Node temp=head;
+
+        while (temp!=null){
+            System.out.print (temp.data +",");
+            temp=temp.next;
+        }
+
+        System.out.print ("\b]");
+    }
+
+     public void  insertAtPosition(int data ,int position){
+
+         Node node = new Node (55);
+
+         Node temp=head;
+
+         for (int i = 0; i < position-1; i++) {
+             temp=temp.next;
+         }
+
+         node.next=temp.next;
+         temp.next=node;
+
+     }
+
+     public void deleteAtBeginning(){
+         head=head.next;
+     }
 }
